8f3ad8db7250cc3d42d94b02b7efe816e8a74d1c
HNgoc <lmtcv1.9@gmail.com>
2024-10-10 20:52:56 +0700

    c

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ const {
   Schema
 } = mongoose;
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 const white_list = ['http://hngoc.kesug.com', 'https://hngoc.kesug.com'];
 const corsOptions = (req, callback) => {
   const origin = req.header('Origin');
