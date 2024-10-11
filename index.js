133922551603d9094f82952f4d3b50e3986e8459
HNgoc <144669382+hngoc-29@users.noreply.github.com>
2024-10-11 16:37:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ const app = express();
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
+app.use(cors({
+    origin: 'https://hngoc.kesug.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.options('/upload', cors());
 //mongo db
 const singSchema = new Schema( {
   name: String,
